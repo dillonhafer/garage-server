e0b1a108815f0476a26b349ea274576ababcac20
Dillon Hafer <dh@dillonhafer.com>
2015-10-24 19:24:40 -0500

    Add HMAC signature verification to requests.

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"github.com/stianeikeland/go-rpio"
 	"net/http"
 	"os"
+	"time"
 )
 
 const Version = "1.0.0"
@@ -14,6 +21,48 @@ var options struct {
 	httpAddr  string
 	pinNumber int
 	version   bool
+}
+
+var sharedSecret = os.Getenv("GARAGE_SECRET")
+
+func CheckMAC(message, messageMAC []byte) bool {
+	mac := hmac.New(sha512.New, []byte(sharedSecret))
+	mac.Write(message)
+	expectedMAC := []byte(hex.EncodeToString(mac.Sum(nil)))
+	return hmac.Equal(messageMAC, expectedMAC)
+}
+
+func verifyTime(decodedJSON []byte) (map[string]int, error) {
+	payload := make(map[string]int)
+	err := json.Unmarshal(decodedJSON, &payload)
+	if err != nil {
+		return nil, err
+	}
+
+	time64 := int64(payload["timestamp"])
+	if (time.Now().Unix() - time64) > 30 {
+		return nil, errors.New("Timestamp is too far in the past")
+	}
+
+	return payload, nil
+}
+
+func toggleSwitch() (err error) {
+	err = rpio.Open()
+	if err != nil {
+		return
+	}
+	pin := rpio.Pin(25)
+	pin.Output()
+	pin.High()
+	time.Sleep(1 * time.Second)
+	pin.Low()
+	return nil
+}
+
+type AppRequest struct {
+	Data      string `json:"data"`
+	Signature string `json:"signature"`
 }
 
 func main() {
@@ -27,6 +76,11 @@ func main() {
 	flag.BoolVar(&options.version, "version", false, "print version and exit")
 
 	flag.Parse()
+
+	if sharedSecret == "" {
+		println("You did not set GARAGE_SECRET env var")
+		os.Exit(0)
+	}
 
 	if options.version {
 		fmt.Printf("garage-server v%v\n", Version)
@@ -42,14 +96,54 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var jsonResp struct {
-			Text string `json:"text"`
+			Text string `json:"status"`
 		}
-		jsonResp.Text = "Open/Close sent"
-		js, err := json.Marshal(jsonResp)
+		jsonResp.Text = "signal received"
+
+		decoder := json.NewDecoder(r.Body)
+		var appRequest AppRequest
+		err := decoder.Decode(&appRequest)
+		if err != nil {
+			panic("Could Not Decode JSON")
+		}
+
+		signature := appRequest.Signature
+		decodedSignature, err := base64.URLEncoding.DecodeString(signature)
+		if err != nil {
+			panic(err)
+		}
+
+		data := appRequest.Data
+		decodedJSON, err := base64.URLEncoding.DecodeString(data)
+		if err != nil {
+			panic(err)
+		}
+
+		verified := CheckMAC(decodedJSON, decodedSignature)
+		if verified {
+			fmt.Println("Signature verified")
+			_, err := verifyTime(decodedJSON)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("Time verified")
+			err = toggleSwitch()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				w.WriteHeader(422)
+				jsonResp.Text = fmt.Sprintf("%s", err)
+			}
+		} else {
+			w.WriteHeader(422)
+			jsonResp.Text = fmt.Sprintf("%s", "Invalid signature")
+		}
+
+		message, err := json.Marshal(jsonResp)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-		w.Write(js)
+		w.Write(message)
 	})
 
 	err := http.ListenAndServe(serveAddress, nil)
